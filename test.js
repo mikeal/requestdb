65fc1ba2cb0d8983bb3d5b56be0e5f71625366c7
Finn <derfinn@gmail.com>
2014-12-02 02:04:02 +0100

    Add tests

--- test.js
+++ test.js
@@ -1,7 +1,29 @@
 var requestdb = require('./')
   , request = requestdb('./testdb')
+  , nock = require('nock')
+  , rimraf = require('rimraf')
+  , t = require('assert')
+  , ok = require('okdone')
   ;
 
-request('http://www.google.com', function (e, resp, body) {
-  console.log(e, resp.statusCode, body.length)
+// nock mocks http endpoints and throws an error on second request
+nock('http://www.mycoolwebsite.io')
+  .get('/')
+  .reply(200, 'Under Construction')
+
+rimraf('./testdb', function () {
+  request('http://www.mycoolwebsite.io/', function (e, resp, body) {
+    t.ifError(e)
+    t.equal(resp.statusCode, 200)
+    t.equal(body, 'Under Construction')
+    ok('first request')
+    request('http://www.mycoolwebsite.io/', function (e, resp, body) {
+      t.ifError(e)
+      t.equal(resp.statusCode, 200)
+      t.equal(body, 'Under Construction')
+      ok('second request cached')
+      ok.done()
+    })
+  })
 })
+
